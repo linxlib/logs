c29025109ba7d30b297d828fab1dd3ff64b69941
kirileec <sulinke1133@gmail.com>
2021-08-17 13:13:33 +0800

    Update sequences.go
    refer to: https://github.com/nine-lives-later/go-windows-terminal-sequences/issues/4  and https://github.com/sirupsen/logrus/issues/1112

--- sequences.go
+++ sequences.go
@@ -27,7 +27,7 @@ func enableVirtualTerminalProcessing(stream syscall.Handle, enable bool) error {
 		mode &^= ENABLE_VIRTUAL_TERMINAL_PROCESSING
 	}
 
-	ret, _, err := setConsoleMode.Call(uintptr(unsafe.Pointer(stream)), uintptr(mode))
+	ret, _, err := setConsoleMode.Call(uintptr(stream), uintptr(mode))
 	if ret == 0 {
 		return err
 	}
